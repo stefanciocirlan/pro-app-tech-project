94490baecdd95848924f94cf13c2125b02e88a83
STEFAN CIOCIRLAN <stefan.ciocirlan@gammainnovation.it>
2022-09-01 22:27:18 +0200

    Constants file added.

---
+++ src/utils/Constants.js
@@ -0,0 +1,4 @@
+export const Home = "Home";
+export const LogIn = "LogIn";
+export const Form = "Form";
+export const LogOut = "LogOut";
--- src/utils/NavLinks.js
+++ src/utils/NavLinks.js
@@ -1,9 +1,10 @@
 import { AppRoutes } from "../routes/routes";
+import { Form, Home, LogIn, LogOut } from "./Constants";
 
 export const navigation = [
-    { name: "Home", href: AppRoutes.Root.path, current: true },
-    { name: "LogIn", href: AppRoutes.LogIn.path, current: false },
-    { name: "Form", href: AppRoutes.Form.path, current: false },
-    { name: "LogOut", href: AppRoutes.LogOut.path, current: false },
+    { name: Home, href: AppRoutes.Root.path, current: true },
+    { name: LogIn, href: AppRoutes.LogIn.path, current: false },
+    { name: Form, href: AppRoutes.Form.path, current: false },
+    { name: LogOut, href: AppRoutes.LogOut.path, current: false },
   ];
   
